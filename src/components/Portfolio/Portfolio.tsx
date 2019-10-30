8335039c3e9fddfcd1553f33926ddad3fcbaf3c8
Eddie <eddie.jostell@gmail.com>
2019-10-30 22:41:38 +0100

    trying to render out portoflio info

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -24,16 +24,33 @@ export default class Portfolio extends React.Component<Props, State> {
   // }
   render() {
     //const { data } = this.props;
-    console.log(this.props.data);
-    const portfolio = this.props.data.map( (port, key) => <PortfolioContent
-       
+    console.log(this.props.data, "ASDF");
+    const portfolio = this.props.data.map((port, key) => <PortfolioContent 
    
-
-    />)
+    />);
 
     return (
       <div className="Portfolio">
         <h1>PROJECTS</h1>
+        <ul className="List">
+          <li className="List-card">
+            <h3>CLOUD 9 REBORN</h3>
+            <span>
+              Made up homepage for the Cloud9 CSGO team from 2016 to 2017.
+            </span>
+            <span>#HTML #CSS/SASS #Javascript</span>
+            <a href="">
+              <span>View project</span>
+            </a>
+          </li>
+          <li className="List-card">TODO</li>
+          <li className="List-card">FOOD-APP</li>
+          <li className="List-card">NEWS HUB</li>
+          <li className="List-card">MOVIEDATABASE</li>
+          <li className="List-card">FOOD-APP</li>
+          <li className="List-card">FOOD-APP</li>
+          <li className="List-card">FOOD-APP</li>
+        </ul>
       </div>
     );
   }
