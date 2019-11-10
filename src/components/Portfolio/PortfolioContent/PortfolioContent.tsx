f4aac5c992ccfe5201ebc558afbb8b9e0fc9c25e
Eddie <eddie.jostell@gmail.com>
2019-11-10 01:54:00 +0100

    updated portfolio page

--- src/components/Portfolio/PortfolioContent/PortfolioContent.tsx
+++ src/components/Portfolio/PortfolioContent/PortfolioContent.tsx
@@ -6,10 +6,11 @@ function PortfolioContent(props: any) {
     <ul className="List">
     <li className="List-card">
       <div className="List-cardImg">
+      <h3 className="title">{props.title}</h3>
       <img src={props.img}/>
       </div>
       <div className="List-cardContent">
-      <h3>{props.title}</h3>
+      <h3 className="title">{props.title}</h3>
         <span>{props.tech}</span>
         <a href={props.link} target="_blank"><span>View project</span></a>
       </div>
