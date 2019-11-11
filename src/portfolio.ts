c009cc621851b12693185532b05b7fdcb558183c
Eddie <eddie.jostell@gmail.com>
2019-11-12 00:43:52 +0100

    added quotes to my ts file.

--- src/portfolio.ts
+++ src/portfolio.ts
@@ -88,4 +88,49 @@ export const contactInfo: ContactItem[] = [
 ];
 
 
+export interface QuoteItem {
+  id: number;
+  quote: string;
+  author: string;
+}
+
+export const quoteInfo: QuoteItem[] = [
+  {
+    id: 1,
+    quote: "A tree falls in the forest, no one puts it on YouTube, did it ever really happen?",
+    author:"Bodhi Point Break 2015"
+  },
+  {
+    id: 2,
+    quote: "Reality can never live up to that fantasy that you have in your head. You'll be much happier if you accept the fact that real life is never perfect",
+    author: "Unknown"
+  },
+  {
+    id: 3,
+    quote: "Of all the things I've lost I miss my mind the most.",
+    author: "Ozzy Osburne."
+  },
+  {
+    id: 4,
+    quote: "I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.",
+    author: "Bruce Lee."
+  },
+  {
+    id: 5,
+    quote: "Love all, trust a few, do wrong to none.",
+    author: "William Shakespeare."
+  },
+  {
+    id: 6,
+    quote: "We need fantasy to survive reality.",
+    author: "Lady Gaga."
+  },
+  {
+    id: 7,
+    quote: "I am become death, the destroyer of worlds.",
+    author: "J. Robert Oppenheimer."
+  }
+]
+
+
 
